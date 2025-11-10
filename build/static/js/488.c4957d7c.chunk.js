1e092a33912a0ed056ce2f82b726faef2244e324
sharjeel <123869349+s-m-sharjeel@users.noreply.github.com>
2025-11-10 18:33:29 +0500

    optimize for mobile deployment

---
+++ build/static/js/488.c4957d7c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbykea_redesign=self.webpackChunkbykea_redesign||[]).push([[488],{488:(e,k,s)=>{new WeakMap;new Set}}]);
+//# sourceMappingURL=488.c4957d7c.chunk.js.map
